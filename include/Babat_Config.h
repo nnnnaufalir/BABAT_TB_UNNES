a02d0646d94227108970ee1feb64edba686f20fe
nnnnaufalir <ilhamnaufalr29@gmail.com>
2024-09-04 17:37:07 +0700

    update all parameter config

--- include/Babat_Config.h
+++ include/Babat_Config.h
@@ -1,6 +1,13 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// SERIAL BAUDRATE
+#define SERIAL_BAUDRATE 115200
+
+// WiFi Credentials
+const char *ssids[] = {"Galaxy A50s", "SSID2", "SSID3", nullptr};
+const char *passwords[] = {"22224444", "password2", "password3", nullptr};
+
 // LOADCELL PIN
 #define LOADCELL_DOUT_PIN 5
 #define LOADCELL_SCK_PIN 18
