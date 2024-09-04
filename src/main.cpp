3f7bf84ae7a33ea4fa77796687977c12f359a5b4
nnnnaufalir <ilhamnaufalr29@gmail.com>
2024-09-04 18:14:23 +0700

    update to use loadcell, timer,OTA,buzzer,wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,75 @@
+// Global Header
 #include <Arduino.h>
-#include <HX711.h>
-#include <LiquidCrystal_I2C.h>
+#include <BabattbTimer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Local Header
+#include "Babat_Config.h"
+#include "Babat_Loadcell.h"
+#include "Babat_OTA.h"
+#include "Babat_Indicator.h"
+#include "WiFiHelper.h"
+
+//---------Timer Class-------------//
+BabattbTimer timer;
+
+//-------Function---------------//
+void printData();
+void loadcell_loop();
+
+//---------Global Variables-----------//
+//--------Obat---------//
+int jumlah_obat;
 
 void setup()
 {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // SERIAL 0
+  Serial.begin(SERIAL_BAUDRATE);
+
+  // Buzzer
+  buzzer_setup();
+  Start_bootingBuzz();
+
+  // WIFI
+  connectToWiFi(ssids, passwords);
+
+  // OTA
+  OTA_setup();
+
+  delay(2000);
+
+  loadcell_setup();
+  End_bootingBuzz();
+
+  //------Timer running------------//
+  timer.setInterval(20, loadcell_loop);
+
+  /*
+  //-------Debug Print-------------//
+  timer.setInterval(500, printData);
+  */
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+
+  reconnectWiFi(ssids, passwords);
+  OTA_loop();
+
+  timer.run();
 }
 
-// put function definitions here:
-int myFunction(int x, int y)
+//-------Running counting drug----------//
+void loadcell_loop()
 {
-  return x + y;
+  jumlah_obat = estimatedCount();
+}
+//-------Debug Print------------//
+void printData()
+{
+  Serial.print(rawWeight());
+  Serial.print(" | ");
+  Serial.print(processedWeight());
+  Serial.print(" | ");
+  Serial.print(jumlah_obat);
+  Serial.println("");
 }
