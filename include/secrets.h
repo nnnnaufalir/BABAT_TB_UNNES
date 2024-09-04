e2b43c9ca773c3c01cdd37f3ac3c73daace977e8
nnnnaufalir <ilhamnaufalr29@gmail.com>
2024-09-04 17:36:51 +0700

    deleted secret.h

--- include/secrets.h
+++
@@ -1,9 +0,0 @@
-// secrets.h
-#ifndef SECRETS_H
-#define SECRETS_H
-
-// WiFi Credentials
-const char *ssids[] = {"Galaxy A50s", "SSID2", "SSID3", nullptr};
-const char *passwords[] = {"22224444", "password2", "password3", nullptr};
-
-#endif // SECRETS_H
